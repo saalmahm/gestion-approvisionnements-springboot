c3949ad8ef7eb1d815a86e902256d9941b79fdd3
salma hamdi <saalmahm51@gmail.com>
2025-10-29 11:59:47 +0100

    Initialisation du projet Spring Boot (gestion des fournisseurs, produits et commandes)

---
+++ gestion-approvisionnements/src/main/java/com/example/gestion_approvisionnements/GestionApprovisionnementsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_approvisionnements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionApprovisionnementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionApprovisionnementsApplication.class, args);
+	}
+
+}
---
+++ gestion-approvisionnements/src/test/java/com/example/gestion_approvisionnements/GestionApprovisionnementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_approvisionnements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionApprovisionnementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
